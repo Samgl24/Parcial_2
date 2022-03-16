ed00a761870764904fce296b09350bff19872be4
Samgl24 <99697505+Samgl24@users.noreply.github.com>
2022-03-15 19:32:30 -0600

    Add files via upload

---
+++ AplicacionconVariasPaginas/AplicacionconVariasPaginas/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AplicacionconVariasPaginas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            frame1.Navigate(typeof(Login));
+        }
+
+        private void boton1_Click(object sender, RoutedEventArgs e)
+        {
+            frame1.Navigate(typeof(Login));
+        }
+
+        private void boton2_Click(object sender, RoutedEventArgs e)
+        {
+            frame1.Navigate(typeof(Acercade));
+        }
+    }
+}
